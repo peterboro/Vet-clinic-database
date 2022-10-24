89d178ffb86fd33d75f1e40c61734027789ce678
Peter Njuguna <Ptahbn@gmail.com>
2022-10-24 17:50:39 +0300

    Create animals table

--- schema.sql
+++ schema.sql
@@ -2,7 +2,7 @@
 
 CREATE TABLE animals (
     id BIGSERIAL NOT NULL PRIMARY KEY,
-    name varchar(100) NOT NULL,
+    name varchar(50) NOT NULL,
     date_of_birth DATE NOT NULL,
     escape_attempts INT NOT NULL,
     neutered BOOLEAN NOT NULL,
