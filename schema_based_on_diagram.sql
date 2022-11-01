b6969241eeeb5cc3d607688f3db9c64a6f63e5bd
Elfin Git <musongoletracy@gmail.com>
2022-11-01 15:10:02 +0200

    create Treatments and Invoice_items tables

--- schema_based_on_diagram.sql
+++ schema_based_on_diagram.sql
@@ -5,16 +5,32 @@ CREATE TABLE patients (
 );
 
 CREATE TABLE medical_histories (
-  id BIGSERIAL NOT NULL PRIMARY KEY,
+  id SERIAL NOT NULL PRIMARY KEY,
   admitted_at TIMESTAMP,
-  status VARCHAR(50) NOT NULL,
+  status VARCHAR(150) NOT NULL,
   patient_id INT REFERENCES patients(id),
 );
 
 CREATE TABLE invoices (
-  id BIGSERIAL NOT NULL PRIMARY KEY,
+  id SERIAL NOT NULL PRIMARY KEY,
   total_amount DECIMAL NOT NULL,
   generated_at TIMESTAMP,
   paid_at TIMESTAMP,
   medical_history_id INT REFERENCES medical_histories(id),
+);
+
+CREATE TABLE treatments (
+  id SERIAL NOT NULL PRIMARY KEY,
+  type VARCHAR(150) NOT NULL,
+  name VARCHAR(150) NOT NULL,
+  invoice_id INT REFERENCES invoices(id),
+);
+
+CREATE TABLE invoice_items (
+  id SERIAL NOT NULL PRIMARY KEY,
+  unit_price DECIMAL NOT NULL,
+  quantity INT NOT NULL,
+  total_amount DECIMAL NOT NULL,
+  invoice_id INT REFERENCES invoices(id),
+  treatment_id INT REFERENCES treatments(id),
 );
