3d6418c6fa5fc70678befcdcf768149173f1100b
Peter Njuguna <Ptahbn@gmail.com>
2022-11-01 16:20:39 +0300

    Create indexes

--- schema_based_on_diagram.sql
+++ schema_based_on_diagram.sql
@@ -34,3 +34,18 @@ CREATE TABLE invoice_items (
   invoice_id INT REFERENCES invoices(id),
   treatment_id INT REFERENCES treatments(id),
 );
+
+CREATE TABLE medical_histories_treatments (
+  id SERIAL NOT NULL PRIMARY KEY,
+  medical_history_id INT REFERENCES medical_histories(id),
+  treatment_id INT REFERENCES treatments(id),
+);
+
+CREATE INDEX patients_id ON patients (id);
+CREATE INDEX medical_histories_id ON medical_histories (id);
+CREATE INDEX invoices_id ON invoices (id);
+CREATE INDEX treatments_id ON treatments (id);
+CREATE INDEX invoice_items_id ON invoice_items (id);
+CREATE INDEX medical_histories_treatments_id ON medical_histories_treatments (id);
+CREATE INDEX medical_histories_treatments_medical_history_id ON medical_histories_treatments (medical_history_id);
+CREATE INDEX medical_histories_treatments_treatment_id ON medical_histories_treatments (treatment_id);
